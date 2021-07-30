0da86ff26a4f80de0f9231e88fbe3bca27ca2048
rjbgoudie <rjbgoudie@users.noreply.github.com>
2021-07-30 20:38:49 +0100

    dmins doesn't exist in lubridate

--- R/tidybrookes.R
+++ R/tidybrookes.R
@@ -1,7 +1,7 @@
 #' @importFrom dplyr select filter mutate relocate distinct group_by ungroup if_else case_when bind_rows arrange left_join first last rename_with slice summarise
 #' @importFrom rlang enquo
 #' @importFrom glue glue
-#' @importFrom lubridate ymd_hms dhours dmins dseconds ddays
+#' @importFrom lubridate ymd_hms dhours dseconds ddays
 #' @importFrom tidyr pivot_wider
 #' @importFrom stringr str_ends str_starts str_replace str_remove
 #' @importFrom tibble tibble as_tibble
