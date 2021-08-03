6ed20c99e19d97b4fc7626ebb7e082abd87100c2
rjbgoudie <rjbgoudie@users.noreply.github.com>
2021-08-03 09:53:55 +0100

    Fixup by adding str_flatten to NAMESPACE

--- R/tidybrookes.R
+++ R/tidybrookes.R
@@ -3,6 +3,6 @@
 #' @importFrom glue glue
 #' @importFrom lubridate ymd_hms dhours dseconds ddays
 #' @importFrom tidyr pivot_wider
-#' @importFrom stringr str_ends str_starts str_replace str_remove str_detect regex
+#' @importFrom stringr str_ends str_starts str_replace str_remove str_detect regex str_flatten
 #' @importFrom tibble tibble as_tibble
 NULL
