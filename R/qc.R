93ad2b92ad77434ac6c1238f283be1709ec9bf06
rjbgoudie <rjbgoudie@users.noreply.github.com>
2022-10-11 20:09:26 +0100

    Add qc_report()

---
+++ R/qc.R
@@ -0,0 +1,26 @@
+qc_report <- function(adm_data,
+                      adt_data,
+                      tests_data,
+                      tests_def,
+                      fsheet_data,
+                      fsheet_def,
+                      output_dir = getwd(),
+                      ...){
+  path <- rmarkdown::render(
+    input = paste0(system.file(package = "tidybrookes"), "/rmd/qc_report.Rmd"),
+    params = list(adm_data = adm_data,
+                  adt_data = adt_data,
+                  tests_data = tests_data,
+                  tests_def = tests_def,
+                  fsheet_data = fsheet_data,
+                  fsheet_def = fsheet_def),
+    output_dir = output_dir,
+    ...
+  )
+  viewer <- getOption("viewer")
+  if (!is.null(viewer)){
+    rstudioapi::viewer(path)
+  } else {
+    utils::browseURL(path)
+  }
+}
