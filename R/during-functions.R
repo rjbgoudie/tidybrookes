9e325526318b7a6b81c06aa982ea9ae6a534d6bd
rjbgoudie <rjbgoudie@users.noreply.github.com>
2022-10-11 19:58:19 +0100

    Add last_during_before_event
    Also add trial functions for handling max_before_hours, but this does
    not work. The design of this function is not good -- better to extend
    more generally to allow this use case (which was designed for the
    steroids/obesity study)

--- R/during-functions.R
+++ R/during-functions.R
@@ -50,6 +50,49 @@ nearest_to_event_during <- function(..., event_datetime, names_suffix = ""){
     names_suffix = glue("nearest_to_{names_suffix}"))
 }
 
+last_during_before_event <- function(..., event_datetime, names_suffix = ""){
+  event_datetime <- enquo(event_datetime)
+  summarise_during(
+    ...,
+    type = "slice",
+    formula =
+      interval(!! event_datetime, datetime) %>%
+      int_length() %>%
+      purrr:::keep(~ .x < 0) %>%
+      which.max(),
+    names_suffix = glue("last_before_{names_suffix}"))
+}
+
+# nearest_to_event_during <- function(...,
+#                                     event_datetime,
+#                                     names_suffix = "",
+#                                     max_before_hours = Inf,
+#                                     max_after_hours = Inf){
+#   event_datetime <- enquo(event_datetime)
+#   summarise_during(
+#     ...,
+#     type = "slice", # TOD REVERT
+#     formula = slice_nearest_within(!! event_datetime,
+#                                    datetime,
+#                                    max_before_hours,
+#                                    max_after_hours)
+#     ,
+#     names_suffix = glue("nearest_to_{names_suffix}"))
+# }
+#
+# slice_nearest_within <- function(datetime, event_datetime, max_before_hours, max_after_hours){
+#   which_min <- interval(event_datetime, datetime) %>%
+#     map(~ .x / hours(1)) %>%
+#     keep(~ .x >= -max_before_hours & .x <= max_after_hours) %>%
+#     map(~abs(.x)) %>%
+#     which.min()
+#   if (length(which_min) == 0){
+#     NA
+#   } else if(length(which_min) == 1){
+#     rep(which_min, n())
+#   }
+# }
+
 max_during <- function(...){
   summarise_during(...,
                    type = "slice",
