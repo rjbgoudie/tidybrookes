16b5f5298b04bb2c7ad8b72a5d9b5214327298a4
rjbgoudie <rjbgoudie@users.noreply.github.com>
2023-10-20 13:33:53 +0100

    Update imported functions

--- R/tidybrookes.R
+++ R/tidybrookes.R
@@ -1,11 +1,12 @@
 #' @importFrom dplyr select filter mutate relocate distinct group_by ungroup if_else case_when bind_rows arrange left_join full_join first last rename_with slice summarise count pull lag lead n
 #' @importFrom rlang enquo as_label inform warn format_error_bullets expr_print
-#' @importFrom glue glue
-#' @importFrom lubridate ymd ymd_hms dhours dseconds dminutes ddays dweeks dyears dmonths interval int_length
+#' @importFrom glue glue glue_collapse
+#' @importFrom lubridate ymd ymd_hms dhours dseconds dminutes ddays dweeks dyears dmonths interval int_length intersect
 #' @importFrom tidyr pivot_wider separate replace_na
 #' @importFrom stringr str_ends str_starts str_replace str_remove str_detect regex str_flatten coll fixed str_replace_all str_sub str_match
 #' @importFrom tibble tibble as_tibble tribble enframe
-#' @importFrom purrr reduce compact walk2
+#' @importFrom purrr reduce compact walk2 map
 #' @importFrom patchwork plot_layout
 #' @importFrom readr read_delim cols col_character col_integer col_double locale col_datetime
+#' @importFrom progress progress_bar
 NULL
