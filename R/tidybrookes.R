9633c64d502b655c2d64d21d6fae78342aa4f8e5
rjbgoudie <rjbgoudie@users.noreply.github.com>
2024-12-20 16:27:03 +0000

    Import more functions from tidyverse

--- R/tidybrookes.R
+++ R/tidybrookes.R
@@ -1,16 +1,17 @@
-#' @importFrom dplyr select filter mutate relocate distinct group_by ungroup if_else case_when bind_rows arrange left_join full_join first last rename_with slice summarise count pull lag lead n
-#' @importFrom rlang enquo as_label expr_print
+#' @importFrom dplyr select filter mutate relocate distinct group_by ungroup if_else case_when bind_rows arrange left_join full_join first last rename_with slice summarise count pull lag lead n rows_update if_any row_number slice_min
+#' @importFrom rlang enquo as_label expr_print is_logical
 #' @importFrom glue glue glue_collapse
-#' @importFrom lubridate ymd ymd_hms dhours dseconds dminutes ddays dweeks dyears dmonths interval int_length intersect
+#' @importFrom lubridate ymd ymd_hms dhours dseconds dminutes ddays dweeks dyears dmonths interval int_length intersect POSIXct
 #' @importFrom tidyr pivot_wider separate replace_na
 #' @importFrom stringr str_ends str_starts str_replace str_remove str_detect regex str_flatten coll fixed str_replace_all str_sub str_match
 #' @importFrom tibble tibble as_tibble tribble enframe
-#' @importFrom purrr reduce compact walk2 map
+#' @importFrom purrr reduce compact walk2 map keep map_at map_dfr is_list
 #' @importFrom patchwork plot_layout
 #' @importFrom readr read_delim cols col_character col_integer col_double locale col_datetime
 #' @importFrom progress progress_bar
 #' @importFrom clock date_time_parse
 #' @importFrom cli cli_alert_info cli_alert_danger cli_alert_warning cli_warn cli_abort
+#' @importFrom vctrs vec_duplicate_id
 NULL
 
 #' @importFrom tibble tibble
